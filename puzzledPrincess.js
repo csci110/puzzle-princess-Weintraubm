73c093018addb46e5a23b78baf396e06183e1e13
Weintraubm <weintraubm@dewv.edu>
2018-10-29 16:50:41 +0000

    Updates via sync script

--- puzzledPrincess.js
+++ puzzledPrincess.js
@@ -17,3 +17,17 @@ class TicTacToe  extends Sprite {   }
 let theBoard = new TicTacToe();
 
 theBoard.takeTurns();
+
+class TicTacToe extends Sprite {
+    constructor(){
+    super();
+    this.name =
+    this.setImage();
+    this.x = 300;
+    this.y = 85;
+    this.SquareSize =150;
+    this.size = 3;
+    this.activeMarker; // variable exists, but value is undefined 
+    
+}
+}
